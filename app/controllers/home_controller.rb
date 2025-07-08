c3115d9227b60764d17e1b098fba9d9330f9a335
michal0002c <michal.kasperek0002c@gmail.com>
2025-07-08 23:39:14 +0200

    Display only active

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!  
   
   def index
-      @ads = Ad.order(created_at: :desc).limit(3)
+    @ads = Ad.where(status: "aktualne").order(created_at: :desc).limit(3)
   end
 end
