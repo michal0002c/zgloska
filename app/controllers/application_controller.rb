d03da674327bf9fe0564e6e9c4b64cfa62f32a24
michal0002c <michal.kasperek0002c@gmail.com>
2025-07-11 23:13:10 +0200

    Refactor main layout, add glass/blur sticky nav, upgrade index/ad cards, add not found page and friendly publish button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   allow_browser versions: :modern
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  private
+
+  def not_found
+    render "errors/not_found", status: :not_found
   end
 end
