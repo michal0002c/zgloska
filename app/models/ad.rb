a26ae12b76d598308eb54c2aaa85c3460fe57e8f
michal0002c <michal.kasperek0002c@gmail.com>
2025-07-11 22:35:06 +0200

    Model validation

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,5 +1,9 @@
 class Ad < ApplicationRecord
   belongs_to :user
 
-  validates :title, :description, :location, :category, :ad_type, presence: true
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 20 }
+  validates :location, presence: true
+  validates :category, presence: true
+  validates :ad_type, presence: true, inclusion: { in: %w[szukam oferuję], message: "musi być 'szukam' lub 'oferuję'" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  has_many :ads
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true
-  has_many :ads
+  validates :username, 
+    presence: true, 
+    uniqueness: true,
+    length: { in: 3..25 },
+    format: { with: /\A[a-zA-Z0-9_]+\z/, message: "może zawierać tylko litery, cyfry i podkreślniki" }
+
 end
